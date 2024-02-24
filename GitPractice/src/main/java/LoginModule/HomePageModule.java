2addb6a03727501a97b6bb40ae6934ffad80f4ac
Pratik <Pratik@DESKTOP-LBO961I>
2024-02-24 12:12:25 +0530

    Check this has gone to github or not...

---
+++ GitPractice/src/main/java/LoginModule/HomePageModule.java
@@ -0,0 +1,8 @@
+package LoginModule;
+
+public class HomePageModule {
+
+	public static void main(String[] args) {
+		System.out.println("Now we are going to start homepage module");
+	}
+}
--- GitPractice/src/main/java/LoginModule/LoginTC.java
+++ GitPractice/src/main/java/LoginModule/LoginTC.java
@@ -4,5 +4,6 @@ public class LoginTC {
 
 	public static void main(String[] args) {
 		System.out.println("Test case 1 done");
+		System.out.println("I have pulled from gitHub ");
 	}
 }
