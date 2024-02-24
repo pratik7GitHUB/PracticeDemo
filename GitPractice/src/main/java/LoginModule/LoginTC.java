12e6b01b482894829de73d2d3be23f2b31d8c397
Pratik <Pratik@DESKTOP-LBO961I>
2024-02-24 12:17:47 +0530

    I want to see what will hapen

--- GitPractice/src/main/java/LoginModule/LoginTC.java
+++ GitPractice/src/main/java/LoginModule/LoginTC.java
@@ -4,5 +4,6 @@ public class LoginTC {
 
 	public static void main(String[] args) {
 		System.out.println("Test case 1 done");
+		System.out.println("If I will pull it from here it would show git conflict");
 	}
 }
